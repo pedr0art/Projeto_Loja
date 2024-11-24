7f2f5809f39c8de9343021c739993715b9786c8e
pedr0art <pedroart3001@gmail.com>
2024-11-24 20:23:25 -0300

    produtos adicionados

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header/Header';
+import Products from './components/Header/Products/Products';
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
