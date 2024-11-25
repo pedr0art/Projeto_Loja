0c471d13add96d1ac6197da1d99baeae1c711bdb
pedr0art <pedroart3001@gmail.com>
2024-11-25 00:12:14 -0300

    loading e busca de produtos

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header/Header';
 import Products from './components/Header/Products/Products';
+import Provider from './context/Provider';
 function App() {
   return (
-    <div>
+    <Provider>
       <Header />
       <Products />
-    </div>
+    </Provider>
   );
 }
 
--- projeto/src/components/Header/SearchBar/SearchBar.jsx
+++ projeto/src/components/Header/SearchBar/SearchBar.jsx
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {BsSearch} from 'react-icons/bs'
 import './SearchBar.css';
+import fetchProducts from '../../../api/fetchProducts';
+import AppContext from '../../../context/AppContext';
 
 function SearchBar() {
 
+    const{setProducts, setLoading} = useContext(AppContext)
     const [searchValue, setsearchValue] = useState('');
 
+    
+
+    const handleSearch = async (event) => {
+        event.preventDefault()
+        setLoading(true)
+        const products = await fetchProducts(searchValue);
+        setProducts(products)
+        setLoading(false)
+        setsearchValue(''); 
+    }
+
     return ( 
-        <form className='search-bar'>
+        <form className='search-bar' onSubmit={handleSearch}>
         <input type="search" 
         value={searchValue}
         placeholder='Buscar produtos'
