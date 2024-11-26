336c35d489a689bbdb1c13b9dcc9ee5ae9b5d48b
pedr0art <pedroart3001@gmail.com>
2024-11-26 18:00:39 -0300

    ajustes

--- projeto/src/pages/SignUp/index.js
+++ projeto/src/pages/SignUp/index.js
@@ -36,7 +36,7 @@ const Signup = () => {
 
   return (
     <C.Container>
-      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Label>LOGIN</C.Label>
       <C.Content>
         <Input
           type="email"
