7844ac63580b2c58f2a69d407065060062ca2623
pedr0art <pedroart3001@gmail.com>
2024-11-25 22:59:30 -0300

    carrinho funcionando

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header/Header/Header';
 import Products from './components/Header/Products/Products';
 import Provider from './context/Provider';
+import Cart from './components/Header/Cart/Cart';
 function App() {
   return (
     <Provider>
       <Header />
       <Products />
+      <Cart />
     </Provider>
   );
 }
--- projeto/src/components/Header/ProductCard/ProductCard.jsx
+++ projeto/src/components/Header/ProductCard/ProductCard.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types'
 import {BsFillCartPlusFill} from 'react-icons/bs'
 import './ProductCard.css'
 import formatCurrency from '../../../utils/formatCurrency';
+import AppContext from '../../../context/AppContext';
 function ProductCard({data}) {
 
     const {title, thumbnail, price} = data;
+
+    const {cartItems, setCartItems} = useContext(AppContext);
+
+    const handleAddCart = () => {setCartItems([...cartItems, data])};
 
     return ( 
         <section className='product-card'>
@@ -18,7 +23,11 @@ function ProductCard({data}) {
                 <h2 className='card__title'>{title}</h2>
             </div>
 
-            <button type='button' className='button__add-cart'>
+            <button 
+                type='button' 
+                className='button__add-cart'
+                onClick={handleAddCart}
+            >
                 <BsFillCartPlusFill />
             </button>
         </section>
